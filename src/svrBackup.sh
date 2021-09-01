c4148d46dfb9e3c21a9e4f888f2fc70f0defa587
上海深普软件.Co.David <leev@smpoo.com>
2021-09-01 18:07:20 +0800

    2021-09-01 18:07:20

---
+++ src/svrBackup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+mkdir -p /smpoo_file/data/back/http/nginx/www
+mkdir -p /smpoo_file/data/back/etc/nginx/conf.d
+mkdir -p /smpoo_file/data/back/node_pj
+
+cp -rf /smpoo_file/http/nginx/www/static /smpoo_file/data/back/http/nginx/www
+cp -rf /smpoo_file/http/nginx/www/app /smpoo_file/data/back/http/nginx/www
+cp -rf /etc/nginx/conf/default.conf /smpoo_file/data/back/etc/nginx/conf.d
+cp -rf /smpoo_file/node_pj/package.json /smpoo_file/data/back/node_pj/
+cp -rf /smpoo_file/node_pj/
+
+function backPj() {
+  echo "准备备份项目服务端配置，请输入项目名称："
+  read -p ""  pjname
+  if [ ! -n "$pjname" ]; then
+    echo "项目名称不能为空"
+    backPj
+  else
+    echo "准备备份"$pjname"项目下的 sys_conf"
+    mkdir -p "/smpoo_file/data/back/node_pj/"$pjname
+		cp -rf "/smpoo_file/node_pj/"$pjname"/sys_conf" "/smpoo_file/data/back/node_pj/"$pjname"/sys_conf"
+		cp -rf "/smpoo_file/node_pj/"$pjname "/smpoo_file/data/back/node_pj/"$pjname
+		rm -rf "/smpoo_file/node_pj/"$pjname"/log"
+  fi
+}

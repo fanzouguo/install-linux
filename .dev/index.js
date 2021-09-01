012086ff69e6c2b92cfac3b2f2eb09e02f9551ab
上海深普软件.Co.David <leev@smpoo.com>
2021-09-01 20:16:50 +0800

    2021-09-01 20:16:50

--- .dev/index.js
+++ .dev/index.js
@@ -1,8 +1,12 @@
 const shelljs = require('shelljs');
 const { tDate, tEcho } = require('tmind-core');
+try {
 
 shelljs.exec('git add .');
 shelljs.exec(`git commit -m "${tDate().format('YYYY-MM-DD hh:mi:ss')}"`);
 shelljs.exec('git push -u origin main');
 
 tEcho('Done!', '提交成功', 'SUCC');
+} catch (err) {
+	tEcho(err, '提交失败', 'ERR');
+}

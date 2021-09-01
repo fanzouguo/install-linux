72081b6165d963d7a119eb451b02982194fe4c45
上海深普软件.Co.David <leev@smpoo.com>
2021-09-01 18:53:42 +0800

    fix bug backup nginx/default.conf in svrBackup

--- src/svrBackup.sh
+++ src/svrBackup.sh
@@ -6,7 +6,7 @@ mkdir -p /smpoo_file/data/back/node_pj
 
 cp -rf /smpoo_file/http/nginx/www/static /smpoo_file/data/back/http/nginx/www
 cp -rf /smpoo_file/http/nginx/www/app /smpoo_file/data/back/http/nginx/www
-cp -rf /etc/nginx/conf/default.conf /smpoo_file/data/back/etc/nginx/conf.d
+cp -rf /etc/nginx/conf.d/default.conf /smpoo_file/data/back/etc/nginx/conf.d/
 cp -rf /smpoo_file/node_pj/package.json /smpoo_file/data/back/node_pj/
 cp -rf /smpoo_file/node_pj/
 

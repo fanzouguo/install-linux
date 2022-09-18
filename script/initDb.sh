5e60f253ae1f12114debeccd7464800ce7250031
上海深普软件.Co.,Ltd <leev@smpoo.com>
2022-09-18 08:58:28 +0800

    移除了install.sh中对初始密码的设定

---
+++ script/initDb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# # 初始化DB配置
+# function initDbMysql() {
+# 	# 获取默认密码
+# 	# PWD_DB_ROOT=`sed '/^USER_DB_ROOT=/!d;s/.*=//' /root/.sysconf`
+# 	# PWD_DB_DEV=`sed '/^USER_DB_DEV=/!d;s/.*=//' /root/.sysconf`
+# 	# PWD_DB_PROD=`sed '/^USER_DB_PROD=/!d;s/.*=//' /root/.sysconf`
+# }
+echo "TODO: 待完成"
--- script/install.sh
+++ script/install.sh
@@ -222,13 +222,6 @@ function installDocker() {
 	systemctl start docker
 	systemctl enable docker
 }
-# 初始化DB配置
-function initDbMysql() {
-	# 获取默认密码
-	PWD_DB_ROOT=`sed '/^USER_DB_ROOT=/!d;s/.*=//' /root/.sysconf`
-	PWD_DB_DEV=`sed '/^USER_DB_DEV=/!d;s/.*=//' /root/.sysconf`
-	PWD_DB_PROD=`sed '/^USER_DB_PROD=/!d;s/.*=//' /root/.sysconf`
-}
 # 环境清理
 function cleanEnv() {
 	tipGreen "环境清理"
